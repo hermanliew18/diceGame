a5b2c530f9fdf125b04528ecd5b8ca2f8f859fef
DML <hermanliew18@gmail.com>
2021-10-19 22:11:22 +0800

    Dicegame

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(Math.random() * 6 + 1); //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1-dice 6
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0].setAttribute("src",randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2)
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Won!"
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Won!"
+}
+else {
+    document.querySelector("h1").innerHTML = "It is a draw!"
+};
